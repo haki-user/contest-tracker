ba08169ff166e96e720b9d3c36f2bf4d6998dbc5
haki-user <adityapratapsingh33@outlook.com>
2023-12-25 18:01:20 +0530

    deployed api

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { Contests } from "./components/Contests";
 
 // axios.defaults.baseURL = "https://codeforces.com/api/";
-axios.defaults.baseURL = "http://localhost:3000/";
+axios.defaults.baseURL = "https://api-contest-tracker.onrender.com/";
 
 function App() {
   return (
