fe2622aa94604e701dcbe23d191709175f5b9f94
haki-user <adityapratapsingh33@outlook.com>
2023-12-31 13:21:41 +0530

    codeforces shifted to server

---
+++ server/src/codechefScrapper.ts
@@ -0,0 +1,72 @@
+// import axios from 'axios';
+// import puppeteer from 'puppeteer';
+// import { IContest } from './types';
+// 
+// const baseURL = 'https://codechef.com';
+// 
+// export const getCodechefContests = async () => {
+//   try {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     await page.goto(baseURL + '/contests');
+//     // tbody.MuiTableBody-root (rows) -> tr.MuiTableRow-root (row) -> (td.MuiTableCell-root (cell) -> div class="jss61 jss66" -> p (contst name)), (td.MuiTableCell-root (cell) -> div class="jss61 jss66" -> div (class="_timer__container_1c9os_518") -> p, p (start date))
+//     const table = 
+//     const tableRows;
+//     const contests: IContest[] = [];
+//     return contests;
+//   } catch(e) {
+//     console.log('codechef error', e);
+//     return [];
+//   }
+// }
+// 
+// getCodechefContests();
+//import axios from 'axios';
+import puppeteer from 'puppeteer';
+//import { IContest } from './types';
+
+const baseURL = 'https://codechef.com';
+
+export const getCodechefContests = async () => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(baseURL + '/contests');
+
+    // Wait for the table to be rendered (you may need to adjust the selector)
+    await page.waitForSelector('tbody.MuiTableBody-root');
+
+    const tableRows = await page.$$('tbody.MuiTableBody-root tr.MuiTableRow-root');
+    const contests = [];
+    console.log(tableRows, tableRows.toString());
+    for (const row of tableRows) {
+      const columns = await row.$$('td.MuiTableCell-root');
+      
+      // Extract contest name
+      const contestName = await columns[0].$eval('div.jss61.jss66 p', (el) => {
+        if(el && el.textContent) el.textContent.trim();
+        return "non";
+      });
+
+      // Extract start date
+      const startDate = await columns[1].$eval('div.jss61.jss66 div._timer__container_1c9os_518 p', (el) => {
+        if(el && el.textContent) el.textContent.trim();
+        return 0;
+      });
+
+      contests.push({
+        name: contestName,
+        startTimeSeconds: startDate || 0,
+      });
+    }
+
+    await browser.close();
+    console.log(contests);
+    return contests;
+  } catch (e) {
+    console.log('CodeChef error', e);
+    return [];
+  }
+};
+
+getCodechefContests();
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { getAtcoderContests } from "./atcoderScrapper";
 import { getLeetcodeContests } from "./leetcodeScrapper";
+import { getCodeforcesContests } from "./codeforcesContest";
 
 const app = express();
 app.use(cors());
@@ -10,7 +11,8 @@ app.use(express.json());
 app.get("/*", async (_, res) => {
   const data = await getAtcoderContests();
   const data2 = await getLeetcodeContests();
-  res.json({ result: [...data, ...data2] });
+  const data3 = await getCodeforcesContests();
+  res.json({ result: [...data, ...data2, ...data3] });
 });
 
 app.listen(3000, () => {
