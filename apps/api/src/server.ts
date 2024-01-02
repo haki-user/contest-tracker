f2d4415de0766050efd47f60d6747a2f8ab877bf
haki-user <adityapratapsingh33@outlook.com>
2024-01-03 00:14:44 +0530

    rate limit api

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -3,7 +3,14 @@ import type { Express } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
 import { router } from "./routes";
+
+const limiter = rateLimit({
+  windowMs: 1000 * 60 * 15, // 15 minutes
+  max: 100, // limit each IP to 500 requests per windowMs
+  standardHeaders: "draft-7",
+});
 
 export const createServer = (): Express => {
   const app = express();
@@ -13,6 +20,7 @@ export const createServer = (): Express => {
     .use(json())
     .use(cors())
     .use(helmet())
+    .use(limiter)
     .use(router);
   return app;
 };
