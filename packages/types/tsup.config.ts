bd77dd33733c8a2467d708c74184fd03410f4c00
haki-user <adityapratapsingh33@outlook.com>
2024-01-11 01:54:02 +0530

    some config changes ejs mts module issue

--- packages/types/tsup.config.ts
+++ packages/types/tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, type Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
   entryPoints: ["src/index.ts"],
-  clean: false,
+  clean: true,
   dts: true,
   format: ["esm"],
   ...options,
